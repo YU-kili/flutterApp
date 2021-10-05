6ebb062ae324cca57ab22083ad54f622f69f94e5
早晚会胖 <3223657165@qq.com>
2021-10-05 18:08:54 +0800

    文本

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,23 @@ class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Welcome to Flutter",
+      title: 'TextWidget',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Word'),
+          title:Text('TextWidget'),
         ),
         body: Center(
-          child: Text('Hello Word!'),
+          child: Text(
+            '项目都是 域名 + 用户 + 项目，试试去掉域名，能来围观的，不会迷项目都是 域名 + 用户 + 项目，试试去掉域名，能来围观的，不会迷项目都是 域名 + 用户 + 项目，试试去掉域名，能来围观的，不会迷',
+            textAlign:TextAlign.left,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 150, 150),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid
+            )
+            
+            ),
           )
       )
     );
